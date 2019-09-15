125dba73f9f7884077b8283e2dc65d34ea7d6bdc
Narinder Rana <Manuj@192.168.1.4>
2019-09-15 16:07:45 +0530

    part

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -4,8 +4,8 @@ import 'package:flutter_restfull_api/main.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static Future<List<dynamic>> getUserList() async{
-    final response=await http.get('${Urls.BASE_API_URL}/users');
+  static Future<List<dynamic>> _get(String url) async{
+    final response=await http.get(url);
     try {
       if (response.statusCode == 200) {
         return json.decode(response.body);
@@ -17,4 +17,31 @@ class ApiService {
       return null;
     }
   }
+
+  static Future<List<dynamic>> getUserList() async{
+    return await _get('${Urls.BASE_API_URL}/users');
+  }
+
+  static Future<List<dynamic>> getPostList() async{
+    return await _get('${Urls.BASE_API_URL}/posts');
+  }
+
+  static Future<dynamic> getPost(int postId) async{
+
+    return await _get('${Urls.BASE_API_URL}/posts/$postId');
+  }
+  static Future<dynamic> getCommentsForPost(int postId) async{
+    return await _get('${Urls.BASE_API_URL}/posts/$postId/comments');
+  }
+
+  static Future<bool> addPost(Map<String, dynamic> post) async {
+    try{
+
+      final response=await http.post('${Urls.BASE_API_URL}/posts', body: post);
+      return response.statusCode==201;
+    }catch(e){
+      return false;
+    }
+  }
+
 }
--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,10 @@ import 'package:flutter_restfull_api/api_service.dart';
 
 void main() => runApp(MyApp());
 
-class Urls{
-  static const BASE_API_URL="https://jsonplaceholder.typicode.com";
+String userId;
+
+class Urls {
+  static const BASE_API_URL = "https://jsonplaceholder.typicode.com";
 }
 
 class MyApp extends StatelessWidget {
@@ -14,12 +16,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Login()
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Login());
   }
 }
 
@@ -29,97 +30,98 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  bool _isLoading = false;
+  TextEditingController _usernameController = new TextEditingController();
 
-  bool _isLoading=false;
-  TextEditingController _usernameController= new TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Log in'),
       ),
-
       body: Padding(
         padding: const EdgeInsets.all(32.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             TextField(
-              decoration: InputDecoration(
-                hintText: 'Username'
-              ),
+              decoration: InputDecoration(hintText: 'Username'),
               controller: _usernameController,
             ),
-            Container(height: 20,),
-            _isLoading ? CircularProgressIndicator() :
-            SizedBox(
-              height: 40,
-              width: double.infinity,
-              child: RaisedButton(
-                color: Colors.blue,
-                child: Text(
-                  'Log in',
-                  style: TextStyle(color: Colors.white),
-                ),
-                onPressed: () async {
-                  setState(() {
-                    _isLoading=true;
-                  });
-                  final users=await ApiService.getUserList();
-                  setState(() {
-                    _isLoading=false;
-                  });
-                  if(users==null){
-                    showDialog(
-                      context: context,
-                      builder: (context){
-                        return AlertDialog(
-                          title: Text("Check your internet connection"),
-                          actions: <Widget>[
-                            FlatButton(
-                              child: Text("Ok"),
-                              onPressed: (){
-                                Navigator.pop(context);
-                              },
-                            )
-                          ],
-                        );
-                      }
-                    );
-                    return;
-                  }else{
-                    final userWithUsernameExists= users.any((u) => u['username']== _usernameController.text);
-                    if(userWithUsernameExists){
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context)=> Posts()
-                        )
-                      );
-                    }else{
-                      showDialog(
-                        context: context,
-                        builder: (context){
-                          return AlertDialog(
-                            title: Text('Incorrect username'),
-                            content: Text('Try with a different username'),
-                            actions: <Widget>[
-                              FlatButton(
-                                child: Text('ok'),
-                                onPressed: (){
-                                  Navigator.pop(context);
-                                },
-                              )
-                            ],
-                          );
-                        }
-                      );
-
-                    }
-                  }
-                },
-              ),
+            Container(
+              height: 20,
             ),
+            _isLoading
+                ? CircularProgressIndicator()
+                : SizedBox(
+                    height: 40,
+                    width: double.infinity,
+                    child: RaisedButton(
+                      color: Colors.blue,
+                      child: Text(
+                        'Log in',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () async {
+                        setState(() {
+                          _isLoading = true;
+                        });
+                        final users = await ApiService.getUserList();
+                        setState(() {
+                          _isLoading = false;
+                        });
+                        if (users == null) {
+                          showDialog(
+                              context: context,
+                              builder: (context) {
+                                return AlertDialog(
+                                  title: Text("Check your internet connection"),
+                                  actions: <Widget>[
+                                    FlatButton(
+                                      child: Text("Ok"),
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                    )
+                                  ],
+                                );
+                              });
+                          return;
+                        } else {
+                          final user=users.where((u)=> u['username']== _usernameController.text).first;
+                          final userWithUsernameExists=user !=null;
+//
+//                          final userWithUsernameExists = users.any(
+//                              (u) => u['username'] == _usernameController.text);
+                          if (userWithUsernameExists) {
+                            userId=user['id'].toString();
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => Posts()));
+                          } else {
+                            showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AlertDialog(
+                                    title: Text('Incorrect username'),
+                                    content:
+                                        Text('Try with a different username'),
+                                    actions: <Widget>[
+                                      FlatButton(
+                                        child: Text('ok'),
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                      )
+                                    ],
+                                  );
+                                });
+                          }
+                        }
+                      },
+                    ),
+                  ),
           ],
         ),
       ),
@@ -130,6 +132,250 @@ class _LoginState extends State<Login> {
 class Posts extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => NewPost(),
+              ));
+        },
+      ),
+      appBar: AppBar(
+        title: Text('Posts'),
+      ),
+      body: FutureBuilder(
+          future: ApiService.getPostList(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              final posts = snapshot.data;
+              return ListView.separated(
+                separatorBuilder: (context, index) {
+                  return Divider(
+                    height: 2,
+                    color: Colors.black,
+                  );
+                },
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(
+                      posts[index]['title'],
+                      style: TextStyle(
+                          color: Colors.black, fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(posts[index]['body']),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => Post(posts[index]['id'])));
+                    },
+                  );
+                },
+                itemCount: posts.length,
+              );
+            }
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }),
+    );
+  }
+}
+
+class Post extends StatelessWidget {
+  final int _id;
+
+  Post(this._id);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Post'),
+      ),
+      body: Column(
+        children: <Widget>[
+          FutureBuilder(
+            future: ApiService.getPost(_id),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                final data = snapshot.data;
+
+                if (data != null) {
+                  return Column(
+                    children: <Widget>[
+                      Text(
+                        data['title'],
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                      Text(data['body']),
+                    ],
+                  );
+                } else {
+                  return Column(
+                    children: <Widget>[
+                      Text(
+                        "--",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                      Text("--"),
+                    ],
+                  );
+                }
+              }
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          ),
+          Container(
+            height: 20,
+          ),
+          Divider(
+            color: Colors.black,
+            height: 3,
+          ),
+          Container(
+            height: 20,
+          ),
+          FutureBuilder(
+            future: ApiService.getCommentsForPost(_id),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                final comments = snapshot.data;
+                return Expanded(
+                  child: ListView.separated(
+                      separatorBuilder: (context, index) => Divider(
+                            height: 2,
+                            color: Colors.black,
+                          ),
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(
+                            comments[index]['name'],
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          subtitle: Text(comments[index]['body']),
+                        );
+                      },
+                      itemCount: comments.length),
+                );
+              }
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class NewPost extends StatefulWidget {
+  @override
+  _NewPostState createState() => _NewPostState();
+}
+
+class _NewPostState extends State<NewPost> {
+
+  bool _isLoading = false;
+
+  final _titleController=TextEditingController();
+  final _bodyController=TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('New post')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextField(
+                controller: _titleController,
+                decoration: InputDecoration(hintText: 'Title')
+            ),
+            TextField(
+              controller: _bodyController,
+                decoration: InputDecoration(hintText: 'Body')
+            ),
+            Container(height: 20,),
+            _isLoading ? CircularProgressIndicator() : SizedBox(
+                height: 50,
+                width: double.infinity,
+                child: RaisedButton(
+                  color: Colors.blue,
+                  child: Text('Submit', style: TextStyle(color: Colors.white)),
+                  onPressed: () {
+                    if(_titleController.text.isEmpty || _bodyController.text.isEmpty){
+                      showDialog(
+                        builder: (context)=> AlertDialog(
+                          title: Text('Failure'),
+                          content: Text('You need to input the title and body of the post'),
+                          actions: <Widget>[
+                            FlatButton(
+                              onPressed: (){Navigator.pop(context);},
+                              child: Text('Ok'),
+                            )
+                          ],
+                        ),
+                        context: context
+                      );
+                      return;
+                    }
+                    final post={
+                      'title': _titleController.text,
+                      'body':_bodyController.text,
+                      'userId':userId
+                    };
+                    setState(() {
+                      _isLoading=true;
+                    });
+
+                    ApiService.addPost(post)
+                        .then((success){
+                          setState(() {
+                            _isLoading=false;
+                          });
+
+                          String title, text;
+                          if(success){
+                            title="success";
+                            text="Your post has been successfully submitted";
+
+                          }
+                          else{
+                            title="Error";
+                            text ="An error occurred while submitting your post";
+                          }
+                          showDialog(
+                            context: context,
+                            builder: (context) => AlertDialog(
+                              title: Text(title),
+                              content: Text(text),
+                              actions: <Widget>[
+                                FlatButton(
+                                  onPressed: (){Navigator.pop(context);},
+                                  child: Text('Ok'),
+                                )
+                              ],
+                            )
+                          );
+                    });
+                  },
+                ))
+          ],
+        ),
+      ),
+    );
   }
 }
